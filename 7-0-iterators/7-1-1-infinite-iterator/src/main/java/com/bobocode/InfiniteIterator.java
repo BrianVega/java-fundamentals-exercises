cfe28ad2205fd34c0fe6d4c0fd9fce972234797b
Brian Vega <BrianVega01@outlook.es>
2024-08-08 12:42:11 -0600

    Infinite iterator

---
+++ 7-0-iterators/7-1-1-infinite-iterator/src/main/java/com/bobocode/InfiniteIterator.java
@@ -0,0 +1,45 @@
+package com.bobocode;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Iterator;
+
+
+public class InfiniteIterator implements Iterator<LocalDate> {
+    private LocalDate currentDate;
+
+    public InfiniteIterator(LocalDate currentDate) {
+        this.currentDate = currentDate;
+        skipWeekends();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public LocalDate next() {
+        return currentDate.plusDays(1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s %s %s, %s", currentDate.getDayOfMonth(), currentDate.getMonth(), currentDate.getYear(), currentDate);
+    }
+
+    public LocalDate getCurrentDate() {
+        return currentDate;
+    }
+
+    private void skipWeekends() {
+        while(isWeekend(currentDate)) {
+            currentDate = currentDate.plusDays(1);
+        }
+    }
+
+    private boolean isWeekend(LocalDate date) {
+        return date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+    }
+}
---
+++ 7-0-iterators/7-1-1-infinite-iterator/src/test/java/InfiniteIteratorTest.java
@@ -0,0 +1,54 @@
+import com.bobocode.InfiniteIterator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+class InfiniteIteratorTest {
+    private final LocalDate NOW = LocalDate.now();
+    private InfiniteIterator businessDaysIterator = new InfiniteIterator(NOW);
+
+    @Test
+    void getInstance() {
+        LocalDate now = LocalDate.now();
+
+        businessDaysIterator = new InfiniteIterator(now);
+
+        assertThat(businessDaysIterator.getCurrentDate()).isEqualTo(now);
+    }
+
+    @Test
+    void initializeOnSaturday() {
+        LocalDate saturday = LocalDate.of(2024, 8, 3);
+        LocalDate monday = LocalDate.of(2024, 8, 5);
+        businessDaysIterator = new InfiniteIterator(saturday);
+        assertThat(businessDaysIterator.getCurrentDate()).isEqualTo(monday);
+    }
+
+    @Test
+    void initializeOnSunday() {
+        LocalDate sunday = LocalDate.of(2024, 8, 4);
+        LocalDate monday = LocalDate.of(2024, 8, 5);
+        businessDaysIterator = new InfiniteIterator(sunday);
+        assertThat(businessDaysIterator.getCurrentDate()).isEqualTo(monday);
+    }
+
+    @Test
+    void hasNext() {
+        assertThat(businessDaysIterator.hasNext()).isTrue();
+    }
+
+    @Test
+    void next() {
+        assertThat(businessDaysIterator.next()
+                .isEqual(businessDaysIterator.getCurrentDate().plusDays(1)))
+                .isTrue();
+    }
+
+    @Test
+    void testToString() {
+        assertThat(businessDaysIterator.toString()).contains( NOW.toString() );
+    }
+}
